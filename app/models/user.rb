0f11becdb60bc0096af1a20dd6bc681f73a5430f
minarai847 <72494683+minarai847@users.noreply.github.com>
2020-10-28 12:45:46 +0900

    新規登録、ログイン、ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
          validates :nickname, presence: true
-         EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-         validates :email, presence: true, uniqueness: true, format: {with: EMAIL}
-         validates :occupation, presence: true
-         validates :position, presence: true
+          # EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+         validates :email, presence: true
+        #  ,uniqueness: true, format: {with: EMAIL}
+           
+         validates :password,format: {with:/\A[a-z\d]{6,100}+\z/i, message: 'Include both letters and numbers'}
          
-        has_many :comments
-         has_many :prototypes
+         validates :first_name,presence: true,format: {with:/\A[一-龥ぁ-ん]/, message: 'Full-width characters'}
+         
+         validates :last_name, presence: true,format: {with:/\A[一-龥ぁ-ん]/,message: 'Full-width characters'}
+         
+         validates :first_name_kana, presence: true,  format: {with:/\A[ァ-ヶー－]+\z/,message: 'Full-width katakana characters'}
+         
+         validates :last_name_kana, presence: true, format: {with:/\A[ァ-ヶー－]+\z/,message: 'Full-width katakana characters'}
 
+         validates :birthday      , presence: true
+
+         has_many :items
+         has_many :orders
 end
