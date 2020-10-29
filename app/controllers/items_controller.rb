237546267090b4b5c3f1954a0b5439a65a33f613
minarai847 <72494683+minarai847@users.noreply.github.com>
2020-10-29 18:09:05 +0900

    商品出品ページへの遷移,アクティブハッシュの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,9 @@
 class ItemsController < ApplicationController
   def index
+    @item = Item.order("created_at DESC")
   end
+
+  def new
+  end
+
 end
---
+++ app/models/item.rb
@@ -0,0 +1,24 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :item_condition_id, numericality: { other_than: 1 }
+  validates :shipping_charges_id, numericality: { other_than: 1 }
+  validates :shipping_area_id, numericality: { other_than: 1 }
+  validates :days_to_ship_id, numericality: { other_than: 1 }
+  validates :price, presence: true
+
+  belongs_to :user 
+  has_one    :order
+  has_one_attached :image
+  
+  belongs_to_active_hash :category
+  belongs_to_active_hash :item_condition
+  belongs_to_active_hash :shipping_area
+  belongs_to_active_hash :shipping_charge
+  belongs_to_active_hash :days_to_ship
+
+end
