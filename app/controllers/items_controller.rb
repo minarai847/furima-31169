9aebe8422dcc98a44f99efa0c612438692bad8fa
minarai847 <72494683+minarai847@users.noreply.github.com>
2020-11-02 19:35:29 +0900

    商品編集ページの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except:[:index,:show]
+  before_action :move_to_index,only: [:edit]
   def index
    
      @items = Item.order("created_at DESC")
@@ -22,8 +23,28 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+  end
+end
+
+
+
   def item_params
     params.require(:item).permit(:image,:name,:description,:category_id,:item_condition_id,:shipping_charge_id,:shipping_area_id,:days_to_ship_id,:price).merge(user_id: current_user.id)
+  end
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless @item.user_id == current_user.id
+      redirect_to action: :index
+    end
   end
 
 end
