090b0001a4debb9f74a59bb0a54d0feda93a9a0c
minarai847 <72494683+minarai847@users.noreply.github.com>
2020-11-02 19:52:33 +0900

    商品編集画面の修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except:[:index,:show]
-  before_action :move_to_index,only: [:edit]
+  before_action :move_to_index,only: [:edit,:update]
   def index
    
      @items = Item.order("created_at DESC")
@@ -24,10 +24,10 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
+   
   end
   def update
-    @item = Item.find(params[:id])
+    
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
