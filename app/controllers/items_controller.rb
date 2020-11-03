44d17c2da846a373271c2dcfbc04e9708f578ec1
minarai847 <72494683+minarai847@users.noreply.github.com>
2020-11-03 10:45:20 +0900

    削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,7 +34,11 @@ class ItemsController < ApplicationController
       render :edit
   end
 end
-
+def destroy
+  item=Item.find(params[:id])
+  item.destroy
+  redirect_to root_path
+end
 
 
   def item_params
